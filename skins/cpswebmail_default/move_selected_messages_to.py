ee10e846e3b3ca38e51ff38e2a47d2b88f9689f1
atchertchian <devnull@localhost>
2004-05-13 17:05:45 +0000

    custom changes

--- skins/cpswebmail_default/move_selected_messages_to.py
+++ skins/cpswebmail_default/move_selected_messages_to.py
@@ -1,6 +1,8 @@
 ##parameters=REQUEST
 ## Script (Python) "move_selected_message_to.py"
 
+from zLOG import LOG, DEBUG
+
 #
 # Update in this case only cause list of checkbox
 #
@@ -9,21 +11,21 @@ mail_session = {}
 
 if REQUEST.form.has_key('IMAPIds'):
     #
-    # If no message selected 
+    # If no message selected
     #
     mail_session['IMAPId']   = REQUEST.form['IMAPIds']
     mail_session['IMAPName'] = IMAPName
     f0 = REQUEST.form['move_to_folder']
     f1 = REQUEST.form['move_to_folder1']
-    if f0 != "Dossiers" and f1 == "--Dossiers--":                          
+    if f0 != "Dossiers" and f1 == "--Dossiers--":
         mail_session['move_to_folder'] = REQUEST.form['move_to_folder']
-        #                                            
-        # Moving messages to the dest folder         
+        #
+        # Moving messages to the dest folder
         #
         REQUEST.SESSION['vm_session'] = mail_session
         context.portal_webMail.moveMessages(REQUEST)
 
-    if f0 == "Dossiers" and f1 != "--Dossiers--":                          
+    if f0 == "Dossiers" and f1 != "--Dossiers--":
         mail_session['move_to_folder'] = REQUEST.form['move_to_folder1']
         #
         # Moving messages to the dest folder
@@ -35,6 +37,4 @@ if REQUEST.form.has_key('IMAPIds'):
 # Redirection to the fetching Interface
 #
 portal_url = context.portal_url()
-REQUEST.RESPONSE.redirect(portal_url 
-    + '/webmail_show?IMAPName=' + IMAPName)
-
+REQUEST.RESPONSE.redirect(portal_url + '/webmail_show?IMAPName=' + IMAPName)
