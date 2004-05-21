0a12fb36d7afdde317ce9774aa62539a04205836
atchertchian <devnull@localhost>
2004-05-21 13:35:53 +0000

    changed call to the method and redirection with a psm

--- skins/cpswebmail_default/create_folders.py
+++ skins/cpswebmail_default/create_folders.py
@@ -4,11 +4,17 @@
 #
 # Creation of the new IMAP folder
 #
-context.portal_webMail.createIMAPFolder(REQUEST)
+res_create = context.portal_webMail.createIMAPFolder(REQUEST.title, REQUEST.IMAPName)
 
-#
+if res_create == 0:
+    #creation sucessfull
+    msg = "imap_folder_creation_successfull"
+else:
+    #creation unsucessfull
+    msg = "imap_folder_creation_unsuccessfull"
+
 # Redirection to the folders edit form
 #
 portal_url = context.portal_url()
-REQUEST.RESPONSE.redirect(portal_url + '/folders_edit_form')
+REQUEST.RESPONSE.redirect(portal_url + '/folders_edit_form?portal_status_message=' + msg)
 
