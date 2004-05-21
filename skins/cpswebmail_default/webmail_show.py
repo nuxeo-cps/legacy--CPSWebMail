5fb834738631d61dfb7d56fdbc2377f0afa82b61
atchertchian <devnull@localhost>
2004-05-21 16:43:51 +0000

    adding sort and order parameters

--- skins/cpswebmail_default/webmail_show.py
+++ skins/cpswebmail_default/webmail_show.py
@@ -7,6 +7,10 @@
 IMAPName = REQUEST.form.get('IMAPName', "INBOX")
 sortmail = REQUEST.form.get('sortmail', "date")
 start    = REQUEST.form.get('start', 0)
+sort     = REQUEST.form.get('sort', "date")
+order    = REQUEST.form.get('order', "asc")
+if order is None:
+    order = "asc"
 
 #
 # Redirection to the fetching page
@@ -15,6 +19,8 @@ portal_url = context.portal_url()
 REQUEST.RESPONSE.redirect(portal_url
     + '/webmail_fetch?IMAPName=' + IMAPName
     + '&sortmail=' + sortmail
+    + '&sort=' + sort
+    + '&order=' + order
     + "&start=" + str(start))
 
 
