33f4044ae44e3bade4d4e8c789e8abce87cddb16
atchertchian <devnull@localhost>
2004-06-15 16:16:49 +0000

    adding method to put links into the personal addressbook storing links, from the global address book

---
+++ skins/cpswebmail_default/addressbook_add_contacts_links_to_privbook.py
@@ -0,0 +1,22 @@
+##parameters=REQUEST
+# $Id: #
+
+from zLOG import LOG, DEBUG
+
+#
+# Take the default address book
+#
+addressbook_name = getattr(REQUEST, 'addressbook_name', '_global')
+addressbook  = context.portal_webMail.getCurrentAddressBook(addressbook_name)
+list_to_add = getattr(REQUEST, 'to_add_to_privbooklinks', [])
+
+#
+# Adding the selected entries
+#
+context.portal_webMail.addressbookAddContactsLinksToPrivBook(addressbook, list_to_add)
+
+#
+# Redirection to the addressbook form
+#
+portal_url = context.portal_url()
+REQUEST.RESPONSE.redirect(portal_url + '/addressBook_form?addressbook_name='+addressbook_name)
