26a89a35b441e32f386ea8a156892582b4eb8f4b
atchertchian <devnull@localhost>
2004-07-08 16:38:13 +0000

    testing if the entry is stored in an indirect directory : if yes, need to change the id to remove the name of the directory

--- skins/cpswebmail_default/addressbook_editcontact.py
+++ skins/cpswebmail_default/addressbook_editcontact.py
@@ -1,6 +1,9 @@
 ##parameters=REQUEST
 ## Script (Python) "addressbook_addcontact.py"
 
+from Products.CMFCore.utils import getToolByName
+from zLOG import LOG, DEBUG
+
 #
 # Reinit of session if on exist
 #
@@ -14,7 +17,16 @@ except:
 #
 portal_url  = context.portal_url()
 addressbook_name = REQUEST.get('addressbook_name', '')
-addressbook = context.portal_webMail.getCurrentAddressBookName(addressbook_name, REQUEST=REQUEST)
+
+addressbook_id = context.portal_webMail.getCurrentAddressBookName(addressbook_name, REQUEST=REQUEST)
 id = REQUEST.get('id', '')
-url = portal_url + '/cpswebmail_directory_entry_edit_form?dirname=' + addressbook + '&id=' + id
+dtool = getToolByName(context, 'portal_directories', None)
+addressbook = getattr(dtool, addressbook_id)
+if addressbook.portal_type == 'CPS Indirect Directory':
+    try:
+        id = id.split('/')[1]
+    except IndexError:
+        pass
+
+url = portal_url + '/cpswebmail_directory_entry_edit_form?dirname=' + addressbook_id + '&id=' + id
 REQUEST.RESPONSE.redirect(url)
