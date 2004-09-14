91b20bac3aa311cfeeab3c56994d1c5a0d8b9fcd
atchertchian <devnull@localhost>
2004-09-14 17:20:38 +0000

    delegating attachment saving to Attachment method

--- skins/cpswebmail_default/export_attachment.py
+++ skins/cpswebmail_default/export_attachment.py
@@ -4,40 +4,29 @@
 
 # extract attachments and create a Document in provate area with data content
 
-TYPE_NAME = 'File'
-
 from Products.CMFCore.utils import getToolByName
+from zLOG import LOG, DEBUG
 
-wmtool = context.portal_webMail
+wmtool = getToolByName(context, 'portal_webMail')
+mtool = getToolByName(context, 'portal_membership')
 vm_session = REQUEST.SESSION.get('vm_session')
 folder = wmtool.getInboxFolder(REQUEST)
 the_id = vm_session['IMAPId']
 message = folder.getIMAPMessage(the_id)
 
-mtool = getToolByName(context, 'portal_membership')
-home_folder = mtool.getHomeFolder()
-
 AttachIdlist = REQUEST.get("AttachIdlist", [])
-for attach_id in AttachIdlist:
-    attachment = message.getAttachments()[int(attach_id) - 1]
-    file_name = attachment.getFilename()
 
-    # create a document object into private area
-    id = string.replace(file_name, ' ', '_')
+if not AttachIdlist:
+    msg = 'cpswebmail_no_attachment_selected'
+else:
+    res = 1
+    for attach_id in AttachIdlist:
+        attachment = message.getAttachments()[int(attach_id) - 1]
+        res = res and attachment.exportToHomeFolder(context)
 
-    # Only If document with same id dont exist
-    if not getattr(home_folder, id, None):
-	data = attachment.getData()
-	content_type = attachment.getContentType()
+    if res:
+        msg = 'cpswebmail_attachments_saved'
+    else:
+        msg = 'cpswebmail_error_export_attachments'
 
-	# Create it
-	home_folder.invokeFactory(TYPE_NAME, id, mime_type=content_type)
-	ob = getattr(home_folder, id)
-
-	# Assign data from attachment
-        # FIXME: that's not the right API
-	ob.edit(file=data, mime_type=content_type)
-
-    # ELSE ?
-
-return RESPONSE.redirect(mtool.getHomeUrl())
+return RESPONSE.redirect(mtool.getHomeUrl()+ '?portal_status_message=' + msg)
