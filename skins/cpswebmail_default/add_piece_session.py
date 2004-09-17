4831c87221bd289933a2f40cac370f3bf85e0e06
atchertchian <devnull@localhost>
2004-09-17 16:52:38 +0000

    change redirection if error while loading the attachment

--- skins/cpswebmail_default/add_piece_session.py
+++ skins/cpswebmail_default/add_piece_session.py
@@ -5,15 +5,19 @@
 # adding the current piece to the SESSION
 #
 context.portal_webMail.saveMailSession(REQUEST)
-context.portal_webMail.addPiece(REQUEST)
-
-IMAPName = REQUEST.form.get('IMAPName', "INBOX")
+res = context.portal_webMail.addPiece(REQUEST)
 
 #
 # Redirection to the edit message form
 #
 portal_url = context.portal_url()
-REQUEST.RESPONSE.redirect(portal_url + '/edit_message?IMAPName=' + IMAPName)
+IMAPName = REQUEST.form.get('IMAPName', "INBOX")
 
+err = res[0]
+if err:
+    args = '?IMAPName=' + IMAPName + '&portal_status_message=' + res[1]
+    REQUEST.RESPONSE.redirect(portal_url + '/edit_message' + args)
+else:
+    REQUEST.RESPONSE.redirect(portal_url + '/edit_message?IMAPName=' + IMAPName)
 
 
