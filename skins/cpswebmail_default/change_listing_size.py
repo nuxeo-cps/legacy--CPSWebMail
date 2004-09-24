8e6ac09a338acde759c6d69f0a7224c6f23de5f8
atchertchian <devnull@localhost>
2004-09-24 10:25:35 +0000

    - Changing the listing size (number of messages per page) did not   have the expected behavior when the listing was not starting from 0,   and the batch message and its links when sometimes wrong.

--- skins/cpswebmail_default/change_listing_size.py
+++ skins/cpswebmail_default/change_listing_size.py
@@ -4,11 +4,12 @@
 context.portal_webMail.setListingSizeSession(listing_size, REQUEST)
 
 #
-# Redirection to the previous page
+# Redirection to the previous page, but back to start=0 page so
+# that the batch message and its links are ok
 #
 portal_url = context.portal_url()
 url = '/webmail_fetch?IMAPName=' + IMAPName +\
-      '&sortmail=' + sortmail + '&start=' + start +\
+      '&sortmail=' + sortmail + '&start=0' +\
       '&sort=' + sort + '&order=' + order
 
 REQUEST.RESPONSE.redirect(portal_url+url)
