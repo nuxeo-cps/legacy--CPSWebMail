62f380f217ed54d543504bfe278fca46284ccbe4
atchertchian <devnull@localhost>
2004-09-29 10:53:36 +0000

    cleaning tabs, replacing them by spaces

--- mime_message.py
+++ mime_message.py
@@ -1,7 +1,7 @@
 ###########################################################################
 #
-#   Copyright (c) 2000 Morten W. Petersen <morten@esol.no>
-#	Copyright (c) 2000 Thingamy Ltd.
+#    Copyright (c) 2000 Morten W. Petersen <morten@esol.no>
+#    Copyright (c) 2000 Thingamy Ltd.
 #
 #    This program is free software; you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -54,82 +54,81 @@ import threading
 
 class mime_part_parse(mimetools.Message):
 
-	def __init__(self, fp):
+    def __init__(self, fp):
 
-		mimetools.Message.__init__(self, fp)
+        mimetools.Message.__init__(self, fp)
 
-		self.filename = 'attachment'
-		self.headers = {}
-		self.media_type, self.sub_type = 'text','plain'
-		self.data = ''
+        self.filename = 'attachment'
+        self.headers = {}
+        self.media_type, self.sub_type = 'text','plain'
+        self.data = ''
 
-		self.encoding=string.strip(string.lower(self.getencoding()))
-		
-		encoding = self.encoding
-		for key in self.keys():
-			self.headers[string.lower(key)] = self[key]
+        self.encoding=string.strip(string.lower(self.getencoding()))
 
-		# We prepare the message for retrieval of the
-		# body.
+        encoding = self.encoding
+        for key in self.keys():
+            self.headers[string.lower(key)] = self[key]
 
-		self.fp.seek(self.startofbody)
+        # We prepare the message for retrieval of the
+        # body.
 
-		if not encoding and self.headers.has_key('content-transfer-encoding'):
+        self.fp.seek(self.startofbody)
 
-			encoding = self.headers['content-transfer-encoding']
+        if not encoding and self.headers.has_key('content-transfer-encoding'):
 
-		if encoding in ('7bit','8bit',''):
+            encoding = self.headers['content-transfer-encoding']
 
-			self.data = self.fp.read()
-		else:
-			if encoding in mimetools.decodetab.keys():
-				tmp = cStringIO.StringIO()
-				#mimetools.decode(self.fp, tmp, encoding)
-				if encoding=="quoted-printable":
-					#if decode is too long, process is stoped
-					mythread=threading.Thread(target=mimetools.decode, args=(self.fp, tmp, encoding))
-					mythread.start()
-					mythread.join(8)
-					self.data = tmp.getvalue()
-					if len(self.data)==0:
-						self.data=self.fp.read()
-				else:
-					mimetools.decode(self.fp, tmp, encoding)
-					self.data = tmp.getvalue()
+        if encoding in ('7bit','8bit',''):
+            self.data = self.fp.read()
+        else:
+            if encoding in mimetools.decodetab.keys():
+                tmp = cStringIO.StringIO()
+                #mimetools.decode(self.fp, tmp, encoding)
+                if encoding=="quoted-printable":
+                    #if decode is too long, process is stoped
+                    mythread=threading.Thread(target=mimetools.decode, args=(self.fp, tmp, encoding))
+                    mythread.start()
+                    mythread.join(8)
+                    self.data = tmp.getvalue()
+                    if len(self.data)==0:
+                        self.data=self.fp.read()
+                else:
+                    mimetools.decode(self.fp, tmp, encoding)
+                    self.data = tmp.getvalue()
 
-				del tmp
-			else:
+                del tmp
+            else:
 
-				raise 'UnknownEncodingException', encoding
+                raise 'UnknownEncodingException', encoding
 
-		parameters = self.getparamnames()		
+        parameters = self.getparamnames()
 
-		if 'filename' in parameters:
+        if 'filename' in parameters:
 
-			self.filename = self.getparam('filename')
+            self.filename = self.getparam('filename')
 
 
-		if self.filename=="attachment":
-			for item in self.keys():
-				if string.find(self.getheader(item), 'filename') != (-1):
-					self.filename=re.sub(r'(.+)(lename=")(.+)(")', r'\3', self.getheader(item))			
+        if self.filename=="attachment":
+            for item in self.keys():
+                if string.find(self.getheader(item), 'filename') != (-1):
+                    self.filename=re.sub(r'(.+)(lename=")(.+)(")', r'\3', self.getheader(item))
 
-		if 'name' in parameters:
-			self.filename = self.getparam('name')
+        if 'name' in parameters:
+            self.filename = self.getparam('name')
 
-		if self.maintype and self.subtype:
+        if self.maintype and self.subtype:
 
-			self.media_type = string.lower(self.maintype)
-			self.sub_type = string.lower(self.subtype)
+            self.media_type = string.lower(self.maintype)
+            self.sub_type = string.lower(self.subtype)
 
-		else:
+        else:
 
-			# We'll have to guess if it's binary or not..
-			# Should we make use of the content-type guesser?
+            # We'll have to guess if it's binary or not..
+            # Should we make use of the content-type guesser?
 
-			if util.binary_regexp.search(self.data):
-				self.media_type, self.sub_type = 'binary','octet-stream'
+            if util.binary_regexp.search(self.data):
+                self.media_type, self.sub_type = 'binary','octet-stream'
 
-				
-		if self.filename=="attachment" and self.media_type=="message"  and self.sub_type == "rfc822":
-			self.filename="forwarded_mail.eml"
+
+        if self.filename=="attachment" and self.media_type=="message"  and self.sub_type == "rfc822":
+            self.filename="forwarded_mail.eml"
